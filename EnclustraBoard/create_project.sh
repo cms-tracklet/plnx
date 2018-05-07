988b876247f77193012d926bb6fa9af81ecce81d
Ryan Knowlton <Ryan.Knowlton@nd.edu>
2018-05-07 14:56:45 +0000

    Reads MAC from eeprom and passes it to Linux

--- EnclustraBoard/create_project.sh
+++ EnclustraBoard/create_project.sh
@@ -117,67 +117,26 @@ ls project-spec/meta-user/recipes-kernel/linux/linux-xlnx/
 # values currently set in the custom kernel beyond the defaults
 # CONFIG_MICREL_PHY=y -- for Ethernet PHY on the ZX2
 # CONFIG_SPI_SPIDEV=y -- probably not needed?
-# CONFIG_RTC_DRV_PCF85063=y -- RTC on ZX2
 # CONFIG_UIO_PDRV_GENIRQ=y -- for xvcServer
 # CONFIG_UIO_DMEM_GENIRQ=y -- probably not needed
+# CONFIG_RTC_DRV_PCF85063=y -- RTC on ZX2
 
 #Need to add this to config to get ethernet to work
 cat >> project-spec/meta-user/recipes-kernel/linux/linux-xlnx/user_*.cfg <<EOF
 CONFIG_KSZ9031=y
 EOF
 
-cat >> project-spec/meta-user/conf/petalinuxbsp.conf <<EOF
-#define CONFIG_EXTRA_ENV_SETTINGS \
-	SERIAL_MULTI \
-	CONSOLE_ARG \
-	PSSERIAL0 \
-	"nc=setenv stdout nc;setenv stdin nc;\0" \
-	"ethaddr=00:0a:35:00:1e:52\0" \
-	"importbootenv=echo \"Importing environment from SD ...\"; " \
-		"env import -t ${loadbootenv_addr} $filesize\0" \
-	"loadbootenv=load mmc $sdbootdev:$partid ${loadbootenv_addr} ${bootenv}\0" \
-	"sd_uEnvtxt_existence_test=test -e mmc $sdbootdev:$partid /uEnv.txt\0" \
-	"uenvboot=" \
-	"if run sd_uEnvtxt_existence_test; then" \
-		"run loadbootenv" \
-		"echo Loaded environment from ${bootenv};" \
-		"run importbootenv; \0" \
-	"sdboot=echo boot Petalinux; run uenvboot ; mmcinfo && fatload mmc 0 ${netstart} ${kernel_img} && bootm \0" \
-	"autoload=no\0" \
-	"clobstart=0x10000000\0" \
-	"netstart=0x10000000\0" \
-	"dtbnetstart=0x11800000\0" \
-	"loadaddr=0x10000000\0" \
-	"boot_img=BOOT.BIN\0" \
-	"load_boot=tftpboot ${clobstart} ${boot_img}\0" \
-	"update_boot=setenv img boot; setenv psize ${bootsize}; setenv installcmd \"install_boot\"; run load_boot ${installcmd}; setenv img; setenv psize; setenv installcmd\0" \
-	"install_boot=mmcinfo && fatwrite mmc 0 ${clobstart} ${boot_img} ${filesize}\0" \
-	"bootenvsize=0x20000\0" \
-	"bootenvstart=0x500000\0" \
-	"eraseenv=sf probe 0 && sf erase ${bootenvstart} ${bootenvsize}\0" \
-	"jffs2_img=rootfs.jffs2\0" \
-	"load_jffs2=tftpboot ${clobstart} ${jffs2_img}\0" \
-	"update_jffs2=setenv img jffs2; setenv psize ${jffs2size}; setenv installcmd \"install_jffs2\"; run load_jffs2 test_img; setenv img; setenv psize; setenv installcmd\0" \
-	"sd_update_jffs2=echo Updating jffs2 from SD; mmcinfo && fatload mmc 0:1 ${clobstart} ${jffs2_img} && run install_jffs2\0" \
-	"install_jffs2=sf probe 0 && sf erase ${jffs2start} ${jffs2size} && " \
-		"sf write ${clobstart} ${jffs2start} ${filesize}\0" \
-	"kernel_img=image.ub\0" \
-	"load_kernel=tftpboot ${clobstart} ${kernel_img}\0" \
-	"update_kernel=setenv img kernel; setenv psize ${kernelsize}; setenv installcmd \"install_kernel\"; run load_kernel ${installcmd}; setenv img; setenv psize; setenv installcmd\0" \
-	"install_kernel=mmcinfo && fatwrite mmc 0 ${clobstart} ${kernel_img} ${filesize}\0" \
-	"cp_kernel2ram=mmcinfo && fatload mmc 0 ${netstart} ${kernel_img}\0" \
-	"dtb_img=system.dtb\0" \
-	"load_dtb=tftpboot ${clobstart} ${dtb_img}\0" \
-	"update_dtb=setenv img dtb; setenv psize ${dtbsize}; setenv installcmd \"install_dtb\"; run load_dtb test_img; setenv img; setenv psize; setenv installcmd\0" \
-	"sd_update_dtb=echo Updating dtb from SD; mmcinfo && fatload mmc 0:1 ${clobstart} ${dtb_img} && run install_dtb\0" \
-	"fault=echo ${img} image size is greater than allocated place - partition ${img} is NOT UPDATED\0" \
-	"test_crc=if imi ${clobstart}; then run test_img; else echo ${img} Bad CRC - ${img} is NOT UPDATED; fi\0" \
-	"test_img=setenv var \"if test ${filesize} -gt ${psize}\\; then run fault\\; else run ${installcmd}\\; fi\"; run var; setenv var\0" \
-	"netboot=tftpboot ${netstart} ${kernel_img} && bootm\0" \
-	"default_bootcmd=run cp_kernel2ram && bootm ${netstart}\0" \
-""
+cat >> project-spec/meta-user/recipes-bsp/u-boot/files/platform-top.h <<EOF
+#undef CONFIG_PREBOOT
+#define CONFIG_PREBOOT	"echo U-BOOT for YUGE;setenv preboot; echo;env import -t 0xFFFFFC00;dhcp"
 EOF
 
+petalinux-build
+
+# Copy our patched FSBL code from the Enclustra design reference
+cp ${BASEDIR}/patched_zx2_ref_fsbl/src/* components/plnx_workspace/fsbl/fsbl/src
+
+# Recompile with patched FSBL. I know, there's a better way to do this...
 petalinux-build
 
 petalinux-package --boot --fsbl images/linux/zynq_fsbl.elf --fpga images/linux/system_top.bit --u-boot  --kernel --force
